c225f3d4b1e8db6f5b7952269650a7774d3c2f4c
Alec Richardson <alec.richardson@mail.missouri.edu>
2018-11-26 17:42:15 -0600

    Added login route and validation

--- backend/Routes/api/users.js
+++ backend/Routes/api/users.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
+const config = require("../../config.js");
 
 // Load Input Validation
 const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 //Load user model
 const User = require("../../models/user");
@@ -44,6 +46,57 @@ router.post("/register", (req, res) => {
         });
       });
     }
+  });
+});
+
+// @route   POST api/users/login
+// @desc    login user / Return JWT Token
+// @access  Public
+router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+  // Check Validation
+  if (!isValid) {
+    console.log(errors);
+    return res.status(400).json(errors);
+  }
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      errors.email = "User not found";
+      return res.status(404).json(errors);
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User matched
+        const payload = {
+          id: user.id,
+          name: user.name,
+          gradeLevel: user.gradeLevel
+        }; // Create JWT payload
+
+        // Sign Token
+        jwt.sign(
+          payload,
+          config.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        errors.password = "Password incorrect";
+        return res.status(400).json(errors);
+      }
+    });
   });
 });
 
